d13e12d0e31b0a64a2b5610e6445bae632a83887
zhuhaow <zhuhaow@gmail.com>
2026-06-21 15:24:21 +0800

    Add frontend linting

---
+++ apps/site/eslint.config.js
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import astro from "eslint-plugin-astro";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["dist/", ".astro/"]
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...astro.configs["flat/recommended"],
+  {
+    files: ["**/*.{js,ts}"],
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: ["src/**/*.astro"],
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      }
+    }
+  }
+];
